619f12a251f84776e403efdb027a0acd856b56de
Akahibe Kingsley <divine10646@gmail.com>
2021-07-24 23:09:00 +0100

    added UI features

---
+++ js/main.js
@@ -0,0 +1,51 @@
+function openMenu() {
+    document.getElementById("header-menu").style.width = "100%";
+}
+
+function closeMenu() {
+    document.getElementById("header-menu").style.width = "0%";
+}
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+            pauseOnHover: false,
+            responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+
+    $('.team-members').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+            pauseOnHover: false,
+            responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
